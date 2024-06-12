a678d5fb0777d246b8636352cf4cd7c08c184878
Aidan O'Hara <114598541+aidanohara12@users.noreply.github.com>
2024-06-12 11:49:11 -0400

    Added App.swift

---
+++ BlackJackGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlackJackGameApp.swift
+//  BlackJackGame
+//
+//  Created by Aidan O'Hara on 6/11/24.
+//
+
+import SwiftUI
+
+@main
+struct BlackJackGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
